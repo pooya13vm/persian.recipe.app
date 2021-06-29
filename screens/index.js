a7f942fa790490dd449047b849ab9718be9aa761
pooya <63674551+pooya13vm@users.noreply.github.com>
2021-06-29 20:50:24 +0300

    Add files via upload

---
+++ screens/index.js
@@ -0,0 +1,7 @@
+export {default as EntreeScreen} from './entreeScreen';
+export {default as Favorite} from './favoriteScreen';
+export {default as HomeScreen} from './HomeScreen';
+export {default as PastryScreen} from './pastryScreen';
+export {default as RecipeScreen} from './recipeScreen';
+export {default as DishSideScreen} from './sideDishScreen';
+export {default as SoupScreen} from './soupScreen';
